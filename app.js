d1406a88de7e25fbff3e854f9a159968d99b29f9
Elio Osés <eliooses@netfilmx.com>
2016-09-24 06:42:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -325,10 +325,10 @@ app.controller('ViewCtrl', ['$scope', '$rootScope', '$http', '$log', '$location'
     torrent.files.forEach(function (file) {
         console.log("file" +file);
         blob = new Blob([file], {type: "video/mp4"}),
-        console.log("file.getBlobURL" +file.getBlobURL);
-        //url = window.URL.createObjectURL(file);
-        //console.log("url" +url);
-        $("#viewer").append('<video id="my-video" class="video-js" controls autoplay width="640" height="264" poster="MY_VIDEO_POSTER.jpg" data-setup="{}"><source id="my-video-source" src="'+blob+'.mp4" type="video/mp4"></video>')        
+        console.log("blob" +blob);
+        url = window.URL.createObjectURL(file);
+        console.log("url" +url);
+        $("#viewer").append('<video id="my-video" class="video-js" controls autoplay width="640" height="264" poster="MY_VIDEO_POSTER.jpg" data-setup="{}"><source id="my-video-source" src="'+url+'" type="video/mp4"></video>')        
 
         /*
       file.appendTo('#viewer')
